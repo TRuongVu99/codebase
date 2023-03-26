18a3097b15ba9d07532ee51b32a1cafacbb4a074
quan.nguyen <anhquan291@gmail.com>
2023-03-26 23:54:45 +0700

    :bug: import svg file

--- metro.config.js
+++ metro.config.js
@@ -4,20 +4,26 @@
  *
  * @format
  */
-const { assetExts, sourceExts } = require('metro-config/src/defaults/defaults')
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-        babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      },
-      resolver: {
-        assetExts: assetExts.filter(ext => ext !== 'svg'),
-        sourceExts: [...sourceExts, 'svg'],
-      },
-    }),
-  },
-}
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
