b3c1e49a7215e77c5cde591a7c90aed4c1fb0766
TruongVu <vuprook999@gmail.com>
2023-12-21 23:50:33 +0700

    vu commit

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 interface IProps {}
 
 const SignInScreen = (props: IProps) => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>index</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
